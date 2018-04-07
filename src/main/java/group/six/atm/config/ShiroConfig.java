1763c681024ae7a04ccbcf5228972eb5f4b9b8ac
溶酶菌 <hayatesa@live.cn>
2018-04-07 23:50:39 +0800

    Shiro不完善配置

--- src/main/java/group/six/atm/config/ShiroConfig.java
+++ src/main/java/group/six/atm/config/ShiroConfig.java
@@ -1,29 +1,93 @@
 package group.six.atm.config;
 
-/*import java.util.HashMap;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.springframework.context.annotation.Bean;*/
-import org.springframework.context.annotation.Configuration;
+import group.six.atm.shiro.UserRealm;
 
 @Configuration
 public class ShiroConfig {
-	
-	/*
+
+	/**
+	 * 注入sessionManager
+	 * 
+	 * @param redisShiroSessionDAO
+	 * @param redisOpen
+	 * @param shiroRedis
+	 * @return
+	 */
+	@Bean("sessionManager")
+	public SessionManager sessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		// 设置session过期时间为1小时(单位：毫秒)，默认为30分钟
+		sessionManager.setGlobalSessionTimeout(60 * 60 * 1000);
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		// 去除地址栏JSESSIONID参数
+		sessionManager.setSessionIdUrlRewritingEnabled(false);
+
+		return sessionManager;
+	}
+
+	/**
+	 * 注入securityManager
+	 * 
+	 * @param userRealm
+	 * @param sessionManager
+	 * @return
+	 */
+	@Bean("securityManager")
+	public SecurityManager securityManager(UserRealm userRealm, SessionManager sessionManager) {
+		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+		securityManager.setRealm(userRealm);
+		securityManager.setSessionManager(sessionManager);
+
+		return securityManager;
+	}
+
+	@Bean("shiroFilter")
+	public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+		ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+		shiroFilter.setSecurityManager(securityManager);
+		shiroFilter.setLoginUrl("/login.jhtml");
+		shiroFilter.setUnauthorizedUrl("/no_access");
+		Map<String, String> filterMap = new LinkedHashMap<>();
+		// 放权请求
+		filterMap.put("/static/**", "anon");
+		filterMap.put("/sign_in", "anon");
+		// 拦截所有请求
+		filterMap.put("/**", "authc");
+		shiroFilter.setFilterChainDefinitionMap(filterMap);
+
+		return shiroFilter;
+	}
+
+	@Bean("lifecycleBeanPostProcessor")
+	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+		return new LifecycleBeanPostProcessor();
+	}
+
 	@Bean
-	public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
-		ShiroFilterFactoryBean sfb = new ShiroFilterFactoryBean();
-		sfb.setSecurityManager(securityManager);
-		sfb.setLoginUrl("login");
-		sfb.setSuccessUrl("index");
-		sfb.setUnauthorizedUrl("unauthorized");
-		Map<String, FormAuthenticationFilter> filters = new HashMap<String, FormAuthenticationFilter>();
-		filters.put("authc", value)
-		sfb.setFilterChainDefinitions("");
-		return null; 
-	}*/
-	
+	public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+		DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+		proxyCreator.setProxyTargetClass(true);
+		return proxyCreator;
+	}
+
+	@Bean
+	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+		AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+		advisor.setSecurityManager(securityManager);
+		return advisor;
+	}
 }
---
+++ src/main/java/group/six/atm/shiro/UserRealm.java
@@ -0,0 +1,72 @@
+package group.six.atm.shiro;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import group.six.atm.entity.User;
+import group.six.atm.service.IUserService;
+
+import java.util.*;
+
+/**
+ * 认证Realm
+ */
+@Component
+public class UserRealm extends AuthorizingRealm {
+	
+	@Autowired
+	private IUserService userService;
+    /**
+     * 授权(验证权限时调用)
+     */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		User user = (User) principals.getPrimaryPrincipal();
+		//存放权限信息
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+		/*switch (user.getType()){
+			case 1:		//运营人员  type==1
+				info.addRole("OPERATOR");
+				break;
+			case 9:		//超级管理员
+				info.addRole("ADMIN");
+				break;
+			default:
+				info.addRole(null);
+		}*/
+
+		return info;
+	}
+
+	/**
+	 * 认证(登录时调用)
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(
+			AuthenticationToken token) throws AuthenticationException {
+		/*String account = (String) token.getPrincipal();
+		// 获取账户登录信息
+		User user = this.userService.getUserByAccount(account);
+
+		//账号不存在
+		if(user == null) {
+			throw new UnknownAccountException("账号或密码不正确");
+		}
+		if(user.getCanuse()==0){
+			 throw new LockedAccountException();
+		}
+		//参数分别是对象，凭证(会拿去和登陆逻辑参数比较)，真实姓名
+		return new SimpleAuthenticationInfo(user,
+				user.getPasswd(), getName());*/
+		return null;
+	}
+
+}
